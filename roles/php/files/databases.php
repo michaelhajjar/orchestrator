72bb42dd8107142ddde7f43cb5483dd9fd2c45c2
Michael Hajjar <michaelhajjar@Michaels-MacBook-Pro-2.local>
2020-02-09 19:11:23 +0100

    Add PHP role with sample CRUD app

---
+++ roles/php/files/databases.php
@@ -0,0 +1,37 @@
+<?php
+class Database
+{
+    private static $dbName = 'miketestdb';
+    private static $dbHost = {{ groups[inv_group_name][0]['ansible_host'] }};
+    private static $dbUsername = 'michael';
+    private static $dbUserPassword = 'hajjar';
+     
+    private static $cont  = null;
+     
+    public function __construct() {
+        die('Init function is not allowed');
+    }
+     
+    public static function connect()
+    {
+       // One connection through whole application
+       if ( null == self::$cont )
+       {     
+        try
+        {
+          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword); 
+        }
+        catch(PDOException $e)
+        {
+          die($e->getMessage()); 
+        }
+       }
+       return self::$cont;
+    }
+     
+    public static function disconnect()
+    {
+        self::$cont = null;
+    }
+}
+?>
